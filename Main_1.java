d36eed8ec2fd4dec89f749184fbf6a2b9d9076b6
Muhammad Syarif Hidayat <92723671+mshidayat66@users.noreply.github.com>
2022-11-15 22:10:55 +0700

    Masyaallah Alhamdulillah

---
+++ Main_1.java
@@ -0,0 +1,15 @@
+
+import com.lingkaran.*;
+
+public class Main_1 {
+    
+    public static void main(String[] args) {
+        Lingkaran circle_satu = new SetengahLingkaran();
+        LuasSetengah luassatu = new LuasSetengah(circle_satu);
+        
+        luassatu.HitungLuas();
+        luassatu.TampilkanHasil();
+        
+     
+    }
+}
