2facbe5c391b1f13be39054d4e43a3e5e9d86ecb
Muhammad Syarif Hidayat <92723671+mshidayat66@users.noreply.github.com>
2022-11-15 15:03:30 +0700

    Add UTS Essay

---
+++ Main_1.java
@@ -0,0 +1,10 @@
+
+import com.Lingkaran.*;
+
+public class Main_1 {
+    
+    public static void main(String[] args) {
+        Lingkaran x = new SetengahLingkaran();
+        LuasSetengahLingkaran lsl = new LuasSetengahLingkaran(x);
+    }
+}
